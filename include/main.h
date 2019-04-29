abf7624f26674c461d50ecbeede09ee7036ded70
Jay <moka7452@gmail.com>
2019-04-29 14:30:06 +0200

    Adding structure

---
+++ include/main.h
@@ -0,0 +1,8 @@
+//
+// Created by Liberty on 2019-04-29.
+//
+
+#ifndef MACHINE_VISION_TUM_MAIN_H
+#define MACHINE_VISION_TUM_MAIN_H
+
+#endif //MACHINE_VISION_TUM_MAIN_H
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Liberty on 2019-04-29.
+//
+
+#include "../include/main.h"
+#include <iostream>
+
+int main(){
+
+
+}
